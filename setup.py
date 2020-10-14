37b039dd7eaa7e00678369eb4afe4a03e29ff46c
Phil Brockman <PhilBrockman@users.noreply.github.com>
2020-10-14 10:59:31 -0700

    updated requirenments

--- setup.py
+++ setup.py
@@ -2,23 +2,18 @@ from distutils.core import setup
 setup(
   name = 'midimusic',         # How you named your package folder (MyLib)
   packages = ['midimusic'],   # Chose the same as "name"
-  version = '0.1',      # Start with a small number and increase it with every change you make
+  version = '0.2',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Automate fetching of midis from MuseNet',   # Give a short description about your library
   author = 'Phil Brockman',                   # Type in your name
-  author_email = 'phil.brockman@gmail.com.com',      # Type in your E-Mail
+  author_email = 'phil.brockman@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/PhilBrockman/musepyt',   # Provide either the link to your github or to your website
   download_url = 'https://github.com/PhilBrockman/musepyt/archive/v_01.tar.gz',    # I explain this later on
   keywords = ['MuseNet', 'midi'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'IPython',
           'pretty_midi',
-          'numpy',
-          'time',
-          'datetime',
-          'json',
-          'requests',
-          'copy'
+          'numpy'
       ],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
